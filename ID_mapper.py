f1b6f6cc95d5a60fa02d52b04c87a6315eef6c95
mcmurryj <jonathan.mcmurry@gmail.com>
2017-06-14 11:50:27 -0700

    Removed scripts for processing Morpheus data and experimental data processing.

--- ID_mapper.py
+++
@@ -1,6 +0,0 @@
-from sys import argv
-file = open(argv[1], "r")
-for line in file:
-   if line[0] == ">":
-      print(line.split("embl-cds:")[1].split(" ")[0])
-      print(line.split("GN=")[1].split(" ")[0])
